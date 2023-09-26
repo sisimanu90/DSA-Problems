0ef529af50bda41f73ace37026ff2f65ffb906a6
Khyati <Khyatik15@gmail.com>
2023-09-26 14:44:30 +0530

    stl

--- STL/set.cpp
+++ STL/set.cpp
@@ -3,6 +3,39 @@
 using namespace std;
 int main(){
     set<int> s;
-    s.insert(2);
+    s.insert(5);
+    s.insert(5);
+    s.insert(0);
+    s.insert(0);
+    s.insert(1);
+    s.insert(6);
+    s.insert(6);
+    s.insert(6);
+//stores unique elements only
+
+    for(auto i:s){
+        cout<<i<<endl;
+    }
+    cout<<endl;
+
+    set<int>::iterator it=s.begin();
+    it++;
+
+    s.erase(it);
+     for(auto i:s){
+        cout<<i<<endl;
+    }
+
+    cout<<"Present or not: "<<s.count(5)<<endl; //count checks if element is present or not
+    cout<<"Present or not: "<<s.count(-5)<<endl;
+
+
+    set<int>::iterator itr=s.find(5); //returns iterator of 5
+    for(auto it=itr ; it!=s.end();it++){
+        cout<<*it<<" ";   
+    }cout<<endl;
+
+    
+
 
 }
